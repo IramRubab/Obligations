ba089af935fc00f64faf553bfead52c92e1b7fad
iramrubab <iram.rubab@uni.lu>
2014-03-24 13:17:32 +0100

    Input for O1

--- lu.snt.serval.obligations.api/src/main/java/lu/snt/serval/obligations/api/Input.java
+++ lu.snt.serval.obligations.api/src/main/java/lu/snt/serval/obligations/api/Input.java
@@ -1,5 +1,14 @@
 package lu.snt.serval.obligations.api;
 
 public class Input{
+    private ECL eCL;
+
+    private Integer personCount;
+
+    private Integer globalTime;
+
+    private OStatus status;
+    private SmartPerson person;
+    private Integer time;
 
 }

d5fb304a3b7e25fbaf85ebb4fb9299a336e5b715
Assaad <assaad.moawad@uni.lu>
2014-03-27 15:57:01 +0100

    update

---
+++ lu.snt.serval.obligations.kevoreeapp/src/main/test/Deployer.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.kevoree.tools.test.KevoreeTestCase;
+
+/**
+ * Created by assaad.moawad on 3/27/2014.
+ */
+public class Deployer extends KevoreeTestCase {
+
+    @Test
+    public void startupChildTest() throws Exception {
+        //System.out.println("ok");
+        bootstrap("node0", "init.kevs");
+
+      /*  waitLog("node0", "node0/child1/* INFO: Bootstrap completed", 10000);
+        exec("node0", "set child1.started = \"false\"");
+        assert (getCurrentModel("node0").findNodesByID("child1").getStarted() == false);
+        waitLog("node0", "node0/* INFO: Stopping nodes[child1]", 5000);
+        exec("node0", "set child1.started = \"true\"");
+        assert (getCurrentModel("node0").findNodesByID("child1").getStarted() == true);
+        waitLog("node0", "node0/child1/* INFO: Bootstrap completed", 10000);*/
+    }
+}

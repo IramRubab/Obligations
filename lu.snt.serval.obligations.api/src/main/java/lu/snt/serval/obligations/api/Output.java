eadb243639efa17b26bd606e058c7d217201f2f1
iramrubab <iram.rubab@uni.lu>
2014-03-28 14:34:08 +0100

    output updated

--- lu.snt.serval.obligations.api/src/main/java/lu/snt/serval/obligations/api/Output.java
+++ lu.snt.serval.obligations.api/src/main/java/lu/snt/serval/obligations/api/Output.java
@@ -1,5 +1,12 @@
 package lu.snt.serval.obligations.api;
 
-public class Output{
+public class Output {
+
+    public boolean helpResponseFlag; //We will use this flag to check if SmartPerson has replied in time
+    public boolean globalTimeFlag; //to check if the global time is over
+    public boolean personTime; //to check if the person time is over, maybe its same as helpResponse flag
+    public boolean PersonCountFlag; //to check if the number of person is reached to 3
+    public boolean doorStatusFlag; //to check if door is Open or CLose
+    public boolean cameraActivationFlag; //to see if camera is active or not
 
 }
